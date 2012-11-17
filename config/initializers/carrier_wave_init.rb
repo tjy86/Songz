1ce7bcf9ec64f4a3dd59588c571527b2a14e75ab
tjy86 <thomasjyang@gmail.com>
2012-11-17 15:35:41 -0500

    adding divs to views

--- config/initializers/carrier_wave_init.rb
+++ config/initializers/carrier_wave_init.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV['AWSSEC'],       # required
     :region                 => 'us-east-1'  # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'wdi-songz'                     # required
+  config.fog_directory  = 'songz12'                     # required
   config.fog_public     = false                                   # optional, defaults to true
   # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
   # config.asset_host     = 'https://assets.example.com'            # optional, defaults to nil
